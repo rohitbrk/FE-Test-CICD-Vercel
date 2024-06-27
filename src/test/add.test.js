50fe392ad6a82ce1a0b57222a67c5fd6156ccf59
Rohith <rohithkumarbathula99@gmail.com>
2024-06-27 20:28:23 +0530

    test fail scenario

--- src/test/add.test.js
+++ src/test/add.test.js
@@ -2,5 +2,5 @@ import { add } from "../utils/add.js";
 import { expect, test } from "@jest/globals";
 
 test("adds 1 + 2 to equal 3", () => {
-  expect(add(1, 2)).toBe(3);
+  expect(add(2, 2)).toBe(3);
 });
